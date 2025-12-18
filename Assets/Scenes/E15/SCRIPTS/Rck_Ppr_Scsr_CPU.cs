1fa20ada476f793c582f2792c10307cbfc9b7e21
Alejandro Pereira <alejandrodavid.pereira@estudiant.enti.cat>
2025-12-18 11:17:37 +0100

    Terminé el 15, empiezo el 16

--- Assets/Scenes/E15/SCRIPTS/Rck_Ppr_Scsr_CPU.cs
+++ Assets/Scenes/E15/SCRIPTS/Rck_Ppr_Scsr_CPU.cs
@@ -4,8 +4,6 @@ public class Rck_Ppr_Scsr_CPU : MonoBehaviour
 {
     public int PlayerA;
     public int PlayerB;
-
-    public int round = 0;
     
     public int playerBwin = 0;
     public int playerAwin = 0;
@@ -13,55 +11,32 @@ public class Rck_Ppr_Scsr_CPU : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-
-        while (round < 5)
-        {
+            do
+            {
             // RANDOM NUMBER CREATOR
-            PlayerA = Random.Range(1, 3);
-            PlayerB = Random.Range(1, 3);
+            PlayerA = Random.Range(1, 4);
+            PlayerB = Random.Range(1, 4);
 
             //A WINS
-            if (PlayerA == 1 && PlayerB == 3) 
-                {
-                playerAwin++;
-                Debug.Log("+1 to A ");
-                }            
-            else if (PlayerA == 2 && PlayerB == 1)
+            if (PlayerA == 1 && PlayerB == 3 || PlayerA == 2 && PlayerB == 1)
             {
                 playerAwin++;
                 Debug.Log("+1 to A ");
             }
             //B WINS
-            else if (PlayerB == 1 && PlayerA == 3)
+            else if (PlayerB == 1 && PlayerA == 3 || PlayerB == 2 && PlayerA == 1)
             {
                 playerBwin++;
                 Debug.Log("+1 to B ");
             }
-            else if (PlayerB == 2 && PlayerA == 1)
-            {
-                playerBwin++;
-                Debug.Log("+1 to B ");   
-            }
             // TABLES
-            else if (PlayerB == 1 && PlayerA == 1)
+            else if (PlayerB == 1 && PlayerA == 1 || PlayerB == 2 && PlayerA == 2 || PlayerB == 3 && PlayerA == 3)
             {
                 Debug.Log("TABLES");
             }
-            else if (PlayerB == 2 && PlayerA == 2)
-            {
-                Debug.Log("TABLES");
-            }
-            else if (PlayerB == 3 && PlayerA == 3)
-            {
-                Debug.Log("TABLES");
-            }
-            round++;
-        }
-
-        if (playerAwin > PlayerB) Debug.Log("CPU Player A Wins");
-        else if (playerAwin < PlayerB) Debug.Log("CPU Player B Wins");
-        else Debug.Log("TABLES");
+        } while (playerAwin < 5 && playerBwin < 5);
+        if (playerAwin == 5) Debug.Log("CPU Player A Wins");
+        else if (playerAwin == 5) Debug.Log("CPU Player B Wins");
     }
 
 

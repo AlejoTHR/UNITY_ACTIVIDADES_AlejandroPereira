5650078b6b51da271fba8ec10dfbd40e6217000f
Alejandro Pereira <alejandrodavid.pereira@estudiant.enti.cat>
2026-01-12 17:40:19 +0100

    Termino E29, Comienza E30

--- Assets/Scenes/E29/SCRIPTS/RandomSpawn_Frefall.cs
+++ Assets/Scenes/E29/SCRIPTS/RandomSpawn_Frefall.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 
 public class RandomSpawn_Frefall : MonoBehaviour
 {
-    private float PosX;
-
+    private int PosX;
+    private int min = -3;
+    private int max = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PosX = UnityEngine.Random.Range(min, max);
+        transform.position = new Vector3(PosX, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        PosX = UnityEngine.Random.Range(-4, -5);
         
 
     }

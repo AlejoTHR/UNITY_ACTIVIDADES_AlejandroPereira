df0707022d3abde0372bed5fdc6a598e4b1eb951
Alejandro Pereira <alejandrodavid.pereira@estudiant.enti.cat>
2026-01-15 22:51:25 +0100

    E56 en progreso

---
+++ Assets/Scenes/E56/SCRIPTS/Destroy_Radius.cs
@@ -0,0 +1,50 @@
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Destroy_Radius : MonoBehaviour
+{
+    public int SPEED = 0;
+    private int detection = 3;
+    LayerMask DefaultRaycastLayers;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+
+        if (Keyboard.current.wKey.IsPressed()) dir.y = 1;
+
+        if (Keyboard.current.sKey.IsPressed()) dir.y = -1;
+
+        if (Keyboard.current.dKey.IsPressed()) dir.x = 1;
+
+        if (Keyboard.current.aKey.IsPressed()) dir.x = -1;
+
+        transform.position += dir * SPEED * Time.deltaTime;
+
+
+
+        Collider2D ObjDetected = Physics2D.OverlapCircle(transform.position, detection, DefaultRaycastLayers, 1f, 3f);
+        
+        Destroy(ObjDetected.gameObject);
+
+    }
+
+    public void ObjectsInRange(float range)
+    {
+        Collider2D ColliderInRange = Physics2D.OverlapCircle(transform.position, range);
+
+        Destroy(ColliderInRange.gameObject);
+    }
+
+
+
+}
